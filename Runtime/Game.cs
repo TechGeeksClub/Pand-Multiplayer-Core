7f01c3f32c10ac73a80dd9be62d5b7325e6f6508
Hamza Oban <hamzaoban3@gmail.com>
2025-10-26 14:43:17 +0300

    Update GameModule structure by consolidating core classes and enhancing definitions for improved functionality

---
+++ Runtime/Game.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace PandNet.Core
+{
+    public class Game : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Game started");
+        }
+    }
+}
